70f1d018e7eab5aecfbd2b6a1bc67f36163a51f8
Leonard Orzol <154551929+llorzol@users.noreply.github.com>
2023-12-21 12:07:51 -0800

    Update project_config.py

--- cgi-bin/project_config.py
+++ cgi-bin/project_config.py
@@ -1,1 +1,39 @@
+#!/usr/bin/env python
+#
+###############################################################################
+# $Id: project_config.py
+#
+# Project:  project_config
+# Purpose:  Script contains the path to the web projects containing the
+#            the groundwater information (collection and waterlevel text files).
+#            
+###############################################################################
+# Copyright (c) Oregon Water Science Center
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+###############################################################################
+
+project_config = {
+    "klamath_wells": '/afs/.usgs.gov/www/staging-or.water/cgi-bin/klamath_wells/data',
+    "harney": '/afs/.usgs.gov/www/or.water/cgi-bin/harney/data',
+    "walla_walla":  '/afs/.usgs.gov/www/or.water/cgi-bin/walla_walla/data',
+    "default": '/afs/.usgs.gov/www/staging-or.water/cgi-bin/klamath_wells/data'
+}
+
 
